bafab7ea03ce1a83c1a66e3e8d32b8d11916c1d6
neelchikhal <neelchikhal14@gmail.com>
2025-02-09 00:22:54 +0530

    update task

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -7,19 +7,25 @@ import { FormGroup } from "react-bootstrap";
 const TaskForm = () => {
   const { id } = useParams();
   const navigate = useNavigate();
-  const [task, setTask] = useState({});
+  const [task, setTask] = useState({
+    title: "",
+    status: "",
+    description: "",
+    dueDate: "",
+  });
   const [error, setError] = useState("");
 
   useEffect(() => {
     if (id) {
       getTaskById(id).then((res) => setTask(res));
       console.log(task);
-      
     }
   }, [id]);
 
-  const validateForm = () => {
-    if (!task.title || !task.details || !task.dueDate) {
+  const validateForm = (task) => {
+    console.log("YOHOHO task for validation", task);
+
+    if (!task.title || !task.description || !task.dueDate) {
       setError("All fields are required.");
       return false;
     }
@@ -30,8 +36,8 @@ const TaskForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log(id);
-    
-    if (!validateForm()) return;
+    console.log("VALIDATE FORM", validateForm(task));
+    if (!validateForm(task)) return;
 
     if (id) await updateTask(id, task);
     else await createTask(task);
@@ -44,48 +50,64 @@ const TaskForm = () => {
       <h2>{id ? "Update Task" : "Create Task"}</h2>
       {error && <p style={{ color: "red" }}>{error}</p>}
       <form onSubmit={handleSubmit}>
-      <FormGroup>
-        <input
-          style={{ width: "50%",margin:"10px" }}
-          className="form-control"
-          type="text"
-          placeholder="Title"
-          value={task.title}
-          onChange={(e) => setTask({ ...task, title: e.target.value })}
-          required
-        />
+        <FormGroup>
+          <input
+            style={{ width: "50%", margin: "10px" }}
+            className="form-control"
+            type="text"
+            placeholder="Title"
+            value={task.title}
+            onChange={(e) => setTask({ ...task, title: e.target.value })}
+            required
+          />
         </FormGroup>
         <FormGroup>
-        <textarea
-          style={{ width: "50%",margin:"10px" }}
-          className="form-control"
-          placeholder="Details"
-          value={task.description}
-          onChange={(e) => setTask({ ...task, details: e.target.value })}
-          rows={4}
-          required
-        />
+          <textarea
+            style={{ width: "50%", margin: "10px" }}
+            className="form-control"
+            placeholder="Details"
+            value={task.description}
+            onChange={(e) => setTask({ ...task, description: e.target.value })}
+            rows={4}
+            required
+          />
         </FormGroup>
         <FormGroup>
-        <input
-          style={{ width: "20%",margin:"10px" }}
-          className="form-control"
-          type="date"
-          value={task.dueDate}
-          onChange={(e) => setTask({ ...task, dueDate: e.target.value })}
-          required
-        />
+          <input
+            style={{ width: "20%", margin: "10px" }}
+            className="form-control"
+            type="date"
+            value={task.dueDate}
+            onChange={(e) => setTask({ ...task, dueDate: e.target.value })}
+            required
+          />
         </FormGroup>
         <FormGroup>
-        <select style={{ width: "20%",margin:"10px" }} value={task.status} onChange={(e) => setTask({ ...task, status: e.target.value })}>
-          <option value="Pending">Pending</option>
-          <option value="Completed">Completed</option>
-        </select>
-        
-        <button className="btn btn-primary" type="submit" onClick={(e)=>handleSubmit(e)}>{id ? "Update Task" : "Create Task"}</button>
+          <select
+            style={{ width: "20%", margin: "10px" }}
+            value={task.status}
+            onChange={(e) => setTask({ ...task, status: e.target.value })}
+          >
+            <option value="Pending">Pending</option>
+            <option value="Completed">Completed</option>
+          </select>
+
+          <button
+            className="btn btn-primary"
+            type="submit"
+            onClick={(e) => handleSubmit(e)}
+          >
+            {id ? "Update Task" : "Create Task"}
+          </button>
         </FormGroup>
       </form>
-      <button style={{ width: "50%",margin:"10px" }} className="btn btn-primary" onClick={() => navigate("/")}>Back</button>
+      <button
+        style={{ width: "50%", margin: "10px" }}
+        className="btn btn-primary"
+        onClick={() => navigate("/")}
+      >
+        Back
+      </button>
     </div>
   );
 };
--- src/services/taskService.jsx
+++ src/services/taskService.jsx
@@ -1,10 +1,10 @@
 import axios from "axios";
-import data from "../db.js"
+import data from "../db.js";
 const API_URL = "http://localhost:8081/api/tasks";
 
 export const fetchTasks = async () => {
-  console.log("fetch data",data);
-  
+  console.log("fetch data", data);
+
   return data;
 
   // return await axios.get(`${API_URL}?userId=${userId}`);
@@ -14,7 +14,7 @@ export const getTaskById = async (id) => {
   //console.log(data);
   const userdata = data.find((task) => task.id === id);
   console.log(userdata);
-  
+
   return userdata;
 };
 
@@ -23,7 +23,19 @@ export const createTask = async (taskData) => {
 };
 
 export const updateTask = async (id, taskData) => {
-  return await axios.put(`${API_URL}/update/${id}`, taskData);
+  // return await axios.put(`${API_URL}/update/${id}`, taskData);
+  const modifiedData = data.map((d) => {
+    if (d.id == id) {
+      d.title = taskData.title;
+      d.status = taskData.status;
+      d.description = taskData.description;
+      d.dueDate = taskData.dueDate;
+    }
+    return d;
+  });
+  console.log("UPDATED ARRAY", modifiedData);
+
+  return modifiedData;
 };
 
 export const deleteTask = async (id) => {
